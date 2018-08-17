17f8ac41dcb19ec287cfa69c9d0b1db557e01251
thebjorn <bp@datakortet.no>
2018-08-17 23:54:52 +0200

    Commonjs, working as expected.

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,14 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.es6.js',
+    entry: './src/index.commonjs.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'dk.js',
+        filename: 'dk.commonjs.js',
         library: 'dk',
-        libraryTarget: "umd",
-        libraryExport: "default",
-        umdNamedDefine: true
+        libraryTarget: "var",
+        // libraryExport: "default",
+        // umdNamedDefine: true
     },
     externals: {jquery: 'jQuery'},
     module: { rules: [{
